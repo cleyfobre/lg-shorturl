d3b5c3cfce099286dfb9f376b6e951becd305ebd
Youngmin Jang <youngmin.jang@eztechfin.co.kr>
2025-02-16 01:36:18 +0900

    update jdk and gradle with test codes

--- src/test/java/com/laundrygo/shorturl/ShorturlApplicationTests.java
+++ src/test/java/com/laundrygo/shorturl/ShorturlApplicationTests.java
@@ -1,13 +1,33 @@
 package com.laundrygo.shorturl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ShorturlApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void getMembers() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+            "/members",
+            String.class
+        );
+
+        // System.out.println(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
 }
